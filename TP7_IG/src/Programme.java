4b86d892256e447dbe7d0d1bc134d81d36de318a
chebihiF <chebihi.f@ucd.ac.ma>
2022-05-22 18:50:54 +0100

    add TP7_IG init commit

---
+++ TP7_IG/src/Programme.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Programme {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(500,500);
+        frame.setBackground(new Color(57, 151, 115));
+        frame.getContentPane().setBackground(new Color(57, 151, 115));
+        frame.setTitle("First frame");
+    }
+}
